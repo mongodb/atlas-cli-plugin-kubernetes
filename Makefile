2204938fa5b08bae7218ce2c00b835f41cb1e09b
Melanija Cvetic <melanija.cvetic@mongodb.com>
2024-12-24 13:40:22 +0000

    Implements lint, unit and fuzz test, docs check, and tidy check

---
+++ Makefile
@@ -0,0 +1,38 @@
+
+CLI_SOURCE_FILES?=./cmd/plugin
+CLI_BINARY_NAME=binary
+CLI_DESTINATION=./bin/$(CLI_BINARY_NAME)
+
+.PHONY: build
+build: ## Generate the binary in ./bin
+	@echo "==> Building $(CLI_BINARY_NAME) binary"
+	go build -o $(CLI_DESTINATION) $(CLI_SOURCE_FILES)
+
+.PHONY: lint
+lint: ## Run linter
+	golangci-lint run
+
+.PHONY: unit-test
+unit-test: ## Run unit-tests
+	@echo "==> Running unit tests..."
+	$(TEST_CMD) --tags="$(UNIT_TAGS)" -race -cover -coverprofile $(COVERAGE) -count=1 ./...
+
+.PHONY: fuzz-normalizer-test
+fuzz-normalizer-test: ## Run fuzz test
+	@echo "==> Running fuzz test..."
+	$(TEST_CMD) -fuzz=Fuzz -fuzztime 50s --tags="$(UNIT_TAGS)" -race ./internal/kubernetes/operator/resources
+
+.PHONY: gen-docs
+gen-docs: ## Generate docs for atlascli commands
+	@echo "==> Generating docs"
+	go run -ldflags "$(LINKER_FLAGS)" ./tools/docs/main.go
+
+.PHONY: check-licenses
+check-licenses: ## Check licenses
+	@echo "==> Running lincense checker..."
+	@build/ci/check-licenses.sh
+
+.PHONY: help
+.DEFAULT_GOAL := help
+help:
+	@grep -h -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ cmd/plugin/main.go
@@ -0,0 +1,47 @@
+// Copyright 2024 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mongodb/atlas-cli-plugin-kubernetes/internal/cli/root"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	cmd := root.Builder()
+
+	completionOption := &cobra.CompletionOptions{
+		DisableDefaultCmd:   true,
+		DisableNoDescFlag:   true,
+		DisableDescriptions: true,
+		HiddenDefaultCmd:    true,
+	}
+	rootCmd := &cobra.Command{
+		DisableFlagParsing: true,
+		DisableAutoGenTag:  true,
+		DisableSuggestions: true,
+		CompletionOptions:  *completionOption,
+	}
+	rootCmd.AddCommand(cmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
