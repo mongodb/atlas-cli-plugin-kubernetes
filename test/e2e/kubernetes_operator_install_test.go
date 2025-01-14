033fb916de5916b210914cc7d41c1cc9cca681ce
Melanija Cvetic <melanija.cvetic@mongodb.com>
2025-01-14 14:00:35 +0000

    Enables test in Evergreen

--- test/e2e/kubernetes_operator_install_test.go
+++ test/e2e/kubernetes_operator_install_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build e2e || (atlas && cluster && kubernetes && local)
+//go:build e2e || (kubernetes && local)
 
 package e2e
 
