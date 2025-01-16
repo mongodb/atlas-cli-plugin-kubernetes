87d399a5cb205d77eafeb6e6e2b91e108ffce427
cveticm <119604954+cveticm@users.noreply.github.com>
2025-01-16 12:42:58 +0000

    test: Adds `config generate` e2e tests (#14)
    * Initial migration
    
    * Comments out FedAuth tests to unblock
    
    * Fix to test
    
    * Disables test which requires fedauth

--- test/e2e/atlas_e2e_test_generator_test.go
+++ test/e2e/atlas_e2e_test_generator_test.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/mongodb/atlas-cli-plugin-kubernetes/test"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	atlasv2 "go.mongodb.org/atlas-sdk/v20241113004/admin"
 )
@@ -35,14 +36,22 @@ const (
 
 // atlasE2ETestGenerator is about providing capabilities to provide projects and clusters for our e2e tests.
 type atlasE2ETestGenerator struct {
-	projectID     string
-	projectName   string
-	clusterName   string
-	clusterRegion string
-	tier          string
-	mDBVer        string
-	enableBackup  bool
-	t             *testing.T
+	projectID            string
+	projectName          string
+	clusterName          string
+	clusterRegion        string
+	serverlessName       string
+	teamName             string
+	teamID               string
+	teamUser             string
+	dbUser               string
+	tier                 string
+	mDBVer               string
+	dataFedName          string
+	streamInstanceName   string
+	streamConnectionName string
+	enableBackup         bool
+	t                    *testing.T
 }
 
 // Logf formats its arguments according to the format, analogous to Printf, and
@@ -58,6 +67,41 @@ func (g *atlasE2ETestGenerator) Logf(format string, args ...any) {
 func newAtlasE2ETestGenerator(t *testing.T) *atlasE2ETestGenerator {
 	t.Helper()
 	return &atlasE2ETestGenerator{t: t}
+}
+
+func (g *atlasE2ETestGenerator) generateTeam(prefix string) {
+	g.t.Helper()
+
+	if g.teamID != "" {
+		g.t.Fatal("unexpected error: team was already generated")
+	}
+
+	var err error
+	if prefix == "" {
+		g.teamName, err = RandTeamName()
+	} else {
+		g.teamName, err = RandTeamNameWithPrefix(prefix)
+	}
+	if err != nil {
+		g.t.Fatalf("unexpected error: %v", err)
+	}
+
+	g.teamUser, err = getFirstOrgUser()
+	if err != nil {
+		g.t.Fatalf("unexpected error retrieving org user: %v", err)
+	}
+	g.teamID, err = createTeam(g.teamName, g.teamUser)
+	if err != nil {
+		g.t.Fatalf("unexpected error creating team: %v", err)
+	}
+	g.Logf("teamID=%s", g.teamID)
+	g.Logf("teamName=%s", g.teamName)
+	if g.teamID == "" {
+		g.t.Fatal("teamID not created")
+	}
+	g.t.Cleanup(func() {
+		deleteTeamWithRetry(g.t, g.teamID)
+	})
 }
 
 // generateProject generates a new project and also registers its deletion on test cleanup.
@@ -91,6 +135,143 @@ func (g *atlasE2ETestGenerator) generateProject(prefix string) {
 	g.t.Cleanup(func() {
 		deleteProjectWithRetry(g.t, g.projectID)
 	})
+}
+
+func (g *atlasE2ETestGenerator) generateEmptyProject(prefix string) {
+	g.t.Helper()
+
+	if g.projectID != "" {
+		g.t.Fatal("unexpected error: project was already generated")
+	}
+
+	var err error
+	if prefix == "" {
+		g.projectName, err = RandProjectName()
+	} else {
+		g.projectName, err = RandProjectNameWithPrefix(prefix)
+	}
+	if err != nil {
+		g.t.Fatalf("unexpected error: %v", err)
+	}
+
+	g.projectID, err = createProjectWithoutAlertSettings(g.projectName)
+	if err != nil {
+		g.t.Fatalf("unexpected error: %v", err)
+	}
+	g.t.Logf("projectID=%s", g.projectID)
+	g.t.Logf("projectName=%s", g.projectName)
+	if g.projectID == "" {
+		g.t.Fatal("projectID not created")
+	}
+
+	g.t.Cleanup(func() {
+		deleteProjectWithRetry(g.t, g.projectID)
+	})
+}
+
+func (g *atlasE2ETestGenerator) generatePrivateEndpoint(provider, region string) {
+	g.t.Helper()
+
+	cliPath, err := AtlasCLIBin()
+	if err != nil {
+		g.t.Fatalf("%v: invalid bin", err)
+	}
+
+	cmd := exec.Command(cliPath,
+		privateEndpointsEntity,
+		provider,
+		"create",
+		"--region",
+		region,
+		"--projectId",
+		g.projectID,
+		"-o=json")
+	cmd.Env = os.Environ()
+	resp, err := test.RunAndGetStdOut(cmd)
+	require.NoError(g.t, err, string(resp))
+	var r atlasv2.EndpointService
+	require.NoError(g.t, json.Unmarshal(resp, &r))
+
+	g.t.Logf("endpointServiceID=%s", r.GetId())
+
+	g.t.Cleanup(func() {
+		g.t.Logf("deleting private endpoint service - ID=%s", r.GetId())
+		cmd := exec.Command(cliPath,
+			privateEndpointsEntity,
+			provider,
+			"delete",
+			r.GetId(),
+			"--projectId",
+			g.projectID,
+			"--force")
+		cmd.Env = os.Environ()
+		resp, err := test.RunAndGetStdOut(cmd)
+		require.NoError(g.t, err, string(resp))
+
+		cmd = exec.Command(cliPath,
+			privateEndpointsEntity,
+			provider,
+			"watch",
+			r.GetId(),
+			"--projectId",
+			g.projectID)
+		cmd.Env = os.Environ()
+
+		resp, err = cmd.CombinedOutput()
+		// We expect a 404 error once the private endpoint has been completely deleted
+		require.Error(g.t, err)
+		assert.Contains(g.t, string(resp), "404")
+	})
+}
+
+func (g *atlasE2ETestGenerator) generateDBUser(prefix string) {
+	g.t.Helper()
+
+	if g.projectID == "" {
+		g.t.Fatal("unexpected error: project must be generated")
+	}
+
+	if g.dbUser != "" {
+		g.t.Fatal("unexpected error: DBUser was already generated")
+	}
+
+	var err error
+	if prefix == "" {
+		g.dbUser, err = RandTeamName()
+	} else {
+		g.dbUser, err = RandTeamNameWithPrefix(prefix)
+	}
+	if err != nil {
+		g.t.Fatalf("unexpected error: %v", err)
+	}
+
+	err = createDBUserWithCert(g.projectID, g.dbUser)
+	if err != nil {
+		g.dbUser = ""
+		g.t.Fatalf("unexpected error: %v", err)
+	}
+	g.t.Logf("dbUser=%s", g.dbUser)
+}
+
+func deleteTeamWithRetry(t *testing.T, teamID string) {
+	t.Helper()
+	deleted := false
+	backoff := 1
+	for attempts := 1; attempts <= maxRetryAttempts; attempts++ {
+		e := deleteTeam(teamID)
+		if e == nil || strings.Contains(e.Error(), "GROUP_NOT_FOUND") {
+			t.Logf("team %q successfully deleted", teamID)
+			deleted = true
+			break
+		}
+		t.Logf("%d/%d attempts - trying again in %d seconds: unexpected error while deleting the team %q: %v", attempts, maxRetryAttempts, backoff, teamID, e)
+		time.Sleep(time.Duration(backoff) * time.Second)
+		backoff *= 2
+	}
+
+	if !deleted {
+		t.Errorf("we could not delete the team %q", teamID)
+	}
 }
 
 func deleteProjectWithRetry(t *testing.T, projectID string) {
@@ -161,6 +342,27 @@ func deleteKeys(t *testing.T, toDelete map[string]struct{}) {
 	}
 }
 
+func (g *atlasE2ETestGenerator) generateServerlessCluster() {
+	g.t.Helper()
+
+	if g.projectID == "" {
+		g.t.Fatal("unexpected error: project must be generated")
+	}
+
+	var err error
+	g.serverlessName, err = deployServerlessInstanceForProject(g.projectID)
+	if err != nil {
+		g.t.Errorf("unexpected error deploying serverless instance: %v", err)
+	}
+	g.t.Logf("serverlessName=%s", g.serverlessName)
+
+	g.t.Cleanup(func() {
+		cliPath, err := AtlasCLIBin()
+		require.NoError(g.t, err)
+		deleteServerlessInstanceForProject(g.t, cliPath, g.projectID, g.serverlessName)
+	})
+}
+
 // generateCluster generates a new cluster and also registers its deletion on test cleanup.
 func (g *atlasE2ETestGenerator) generateCluster() {
 	g.t.Helper()
@@ -193,5 +395,87 @@ func (g *atlasE2ETestGenerator) generateCluster() {
 		if e := deleteClusterForProject(g.projectID, g.clusterName); e != nil {
 			g.t.Errorf("unexpected error deleting cluster: %v", e)
 		}
+	})
+}
+
+func (g *atlasE2ETestGenerator) generateDataFederation() {
+	var err error
+	g.t.Helper()
+
+	if g.projectID == "" {
+		g.t.Fatal("unexpected error: project must be generated")
+	}
+
+	g.dataFedName, err = createDataFederationForProject(g.projectID)
+	storeName := g.dataFedName
+	if err != nil {
+		g.Logf("projectID=%q, dataFedName=%q", g.projectID, g.dataFedName)
+		g.t.Errorf("unexpected error deploying data federation: %v", err)
+	} else {
+		g.Logf("dataFedName=%q", g.dataFedName)
+	}
+
+	g.t.Cleanup(func() {
+		g.Logf("Data Federation cleanup %q\n", storeName)
+
+		cliPath, err := AtlasCLIBin()
+		require.NoError(g.t, err)
+
+		deleteDataFederationForProject(g.t, cliPath, g.projectID, storeName)
+		g.Logf("data federation %q successfully deleted", storeName)
+	})
+}
+
+func (g *atlasE2ETestGenerator) generateStreamsInstance(name string) {
+	g.t.Helper()
+
+	if g.projectID == "" {
+		g.t.Fatal("unexpected error: project must be generated")
+	}
+
+	var err error
+	g.streamInstanceName, err = createStreamsInstance(g.t, g.projectID, name)
+	instanceName := g.streamInstanceName
+	if err != nil {
+		g.Logf("projectID=%q, streamsInstanceName=%q", g.projectID, g.streamInstanceName)
+		g.t.Errorf("unexpected error deploying streams instance: %v", err)
+	} else {
+		g.Logf("streamsInstanceName=%q", g.streamInstanceName)
+	}
+
+	g.t.Cleanup(func() {
+		g.Logf("Streams instance cleanup %q\n", instanceName)
+
+		require.NoError(g.t, deleteStreamsInstance(g.t, g.projectID, instanceName))
+		g.Logf("streams instance %q successfully deleted", instanceName)
+	})
+}
+
+func (g *atlasE2ETestGenerator) generateStreamsConnection(name string) {
+	g.t.Helper()
+
+	if g.projectID == "" {
+		g.t.Fatal("unexpected error: project must be generated")
+	}
+
+	if g.streamInstanceName == "" {
+		g.t.Fatal("unexpected error: streams instance must be generated")
+	}
+
+	var err error
+	g.streamConnectionName, err = createStreamsConnection(g.t, g.projectID, g.streamInstanceName, name)
+	connectionName := g.streamConnectionName
+	if err != nil {
+		g.Logf("projectID=%q, streamsConnectionName=%q", g.projectID, g.streamConnectionName)
+		g.t.Errorf("unexpected error deploying streams instance: %v", err)
+	} else {
+		g.Logf("streamsConnectionName=%q", g.streamConnectionName)
+	}
+
+	g.t.Cleanup(func() {
+		g.Logf("Streams connection cleanup %q\n", connectionName)
+
+		require.NoError(g.t, deleteStreamsConnection(g.t, g.projectID, g.streamInstanceName, connectionName))
+		g.Logf("streams connection %q successfully deleted", connectionName)
 	})
 }
