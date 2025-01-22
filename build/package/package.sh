1b11fe46e7c62ea8eae4b7eb31ee03d7e5c75162
Melanija Cvetic <melanija.cvetic@mongodb.com>
2025-01-22 14:10:09 +0000

    Fix to version in manifest

--- build/package/package.sh
+++ build/package/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2022 MongoDB Inc
+# Copyright 2025 MongoDB Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,25 +17,8 @@
 set -Eeou pipefail
 
 export GOROOT="${GOROOT:?}"
-# export NOTARY_SERVICE_URL=${notary_service_url:?}
-# export MACOS_NOTARY_KEY=${notary_service_key_id:?}
-# export MACOS_NOTARY_SECRET=${notary_service_secret:?}
 export GORELEASER_KEY=${goreleaser_key:?}
 export VERSION_GIT=${version:?}
 
-# if [[ "${unstable-}" == "-unstable" ]]; then
-# 	# avoid race conditions on the notarization step by using `-p 1`
-# 	./bin/goreleaser --config "build/package/.goreleaser.yml" --clean --release-notes "CHANGELOG.md" -p 1 --snapshot
-# else
 # avoid race conditions on the notarization step by using `-p 1`
 ./bin/goreleaser --config "build/package/.goreleaser.yml" --clean -p 1
-# fi
-
-# check that the notarization service signed the mac binaries
-# SIGNED_FILE_NAME=atlas-cli-plugin-kubernetes_macos_signed.zip
-# if [[ -f "dist/$SIGNED_FILE_NAME" ]]; then
-# 	echo "$SIGNED_FILE_NAME exists. The Mac notarization service has run."
-# else
-# 	echo "ERROR: $SIGNED_FILE_NAME does not exist. The Mac notarization service has not run."
-# 	exit 1 # ERROR
-# fi
