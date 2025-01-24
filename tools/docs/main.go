1be6f6b02e8b7ff8b9e1c4389b242bb3fadee416
Melanija Cvetic <119604954+cveticm@users.noreply.github.com>
2025-01-24 17:43:54 +0000

    chore: Improve docs generation (#23)
    * Changes docs gen tool
    
    * Adjusts copybara actions
    
    * Enable copybara in patch for testing
    
    * Fixes docs code health check
    
    * Fixes copybara template
    
    * Test copybara
    
    * fix
    
    * Disable patch testing
    
    * Generates Inherited Options
    
    * Enable testing
    
    * fix test
    
    * test
    
    * Removes test

--- tools/docs/main.go
+++ tools/docs/main.go
@@ -41,11 +41,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	pluginBuilder := kubernetes.Builder()
+	pluginBuilder := mockAtlasBuilder()
 
 	setDisableAutoGenTag(pluginBuilder)
 
 	if err := cobra2snooty.GenTreeDocs(pluginBuilder, "./docs/command"); err != nil {
 		log.Fatal(err)
 	}
+
+	filePath := "./docs/command/atlas.txt"
+
+	err := os.Remove(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// mockAtlasBuilder provides a root command to the kubernetes tasks.
+// This ensures that docs generated follow the same structure as in
+// AtlasCLI.
+func mockAtlasBuilder() *cobra.Command {
+	const use = "atlas"
+
+	cmd := &cobra.Command{
+		Use: use,
+	}
+
+	cmd.AddCommand(kubernetes.Builder())
+	// PersistentFlags used to generate 'Inherited Options'
+	cmd.PersistentFlags().StringP("profile", "P", "", "Name of the profile to use from your configuration file. To learn about profiles for the Atlas CLI, see https://dochub.mongodb.org/core/atlas-cli-save-connection-settings.")
+	return cmd
 }
