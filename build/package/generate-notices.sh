2d66e81f99ee817a7dd10ea48074eb3555c03196
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2026-05-06 10:57:38 +0200

    Use go tool go-licenses instead of go install
    go install github.com/google/go-licenses@latest resolves to v1.6.0,
    which fails on stdlib packages with Go 1.21+. The parent atlas-cli
    project already uses the go tool directive pattern (v2.0.1) which
    handles this correctly.
    
    Add go-licenses/v2 as a tool directive in go.mod and switch
    generate-notices.sh to use go tool go-licenses.

--- build/package/generate-notices.sh
+++ build/package/generate-notices.sh
@@ -16,8 +16,6 @@
 
 set -Eeou pipefail
 
-go install github.com/google/go-licenses@latest
-
-go-licenses save "github.com/mongodb/atlas-cli-plugin-kubernetes/cmd/plugin" --save_path=third_party_notices
+go tool go-licenses save "github.com/mongodb/atlas-cli-plugin-kubernetes/cmd/plugin" --save_path=third_party_notices
 # For HCL, a dependency of viper, go-license adds the source code with restricted permissions, this is a problem for some linux users
 sudo chmod -R 755 ./third_party_notices
